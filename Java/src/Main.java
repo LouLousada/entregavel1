3a110c4b640cf7a6862b881ec80d8bd0e607cb90
LouLousada <marilousada7@gmail.com>
2024-09-24 18:52:08 -0300

    aaaaaaaaaaaa

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,25 +1,19 @@
 public class Main {
   public static void main(String[] args) {
-    EhPrimo a = new EhPrimo();
-    a.EPrimo();
-
-      
-
-
-          
-
-          Contagem d = new Contagem();
+    //EhPrimo a = new EhPrimo();
+    //a.EPrimo(); 
+    Contagem d = new Contagem();
              d.contarAte ();
 
-           SomaDeNumeros e = new SomaDeNumeros();
-             e.SomaDeNumeros();
+           //SomaDeNumeros e = new SomaDeNumeros();
+             //e.SomaDeNumeros();
 
     
 
-           MDC g = new MDC();
-           g.MDC();
+           //MDC g = new MDC();
+           //g.MDC();
 
-          Fibonacci h = new Fibonacci();
-          h.Fibonacci();     
+          //Fibonacci h = new Fibonacci();
+          //h.Fibonacci();     
   }
 }
